d975fd56d594ba9ea8ed01cef364f97c008fd43e
Brandon Littell <branli@microsoft.com>
2019-11-07 13:42:02 -0800

    Fix gpa function

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -42,7 +42,7 @@ function dev { Set-Location "C:\dev" }
 
 function gfa { git fetch --all -p }
 
-function gpa { git branch --merged | sls -n '(^\*|master|dev)' | %{&"git branch -D" $_}}
+function gpa { git branch --merged | sls -n '(^\*|master|dev)' | %{$_.toString().trim()} | %{git branch -D $_}}
 
 function hosts { Start-Process $editor -ArgumentList "-multiInst -notabbar -nosession C:\WINDOWS\system32\drivers\etc\hosts" -Verb runAs }
 
